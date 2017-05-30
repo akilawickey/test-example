2b30105f73c7475e32b4f33046341f2fbb4c4b73
Akila Wickey <asak999@gmail.com>
2017-05-30 23:53:49 +0530

    Create test-example.c

---
+++ test-example.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("bb-example:\n");
+    printf("    This is an example application showing how to add custom software to a bitbake recipe.\n");
+    printf("    The tutorial can be found at http://stevephillips.me/blog/adding-custom-software-to-bitbake-oe-core.\n");
+    return 0;
+}
